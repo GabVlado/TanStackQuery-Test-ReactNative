75dd40a6b16bc2ebb8ddcfa823875c7e1d2b0ad8
GabVlad <gv.valdivieso2309@gmail.com>
2024-08-26 23:05:49 -0500

    React query devtools plugin

--- api/movies.ts
+++ api/movies.ts
@@ -1,12 +1,19 @@
+
+
+const headers = {
+
+    accept: "application/json",
+    Authorization:"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmJlODM0ZmVmZjBmMjMyZThkM2RmNmYyMjk4MTU0YyIsIm5iZiI6MTcyNDE5NzE5NC4xODgwNjEsInN1YiI6IjYyM2JkMjkzOTQ1YzIwMDA5MDIyMDcxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IbeMZsg1rrRtoiTbwmJfrSdN_Lazr_izEr5ARQkyHyE",
+
+}
+
+
 export const fetchTopRatedMovies = async () => {
   const url =
     "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1";
   const options = {
     method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmJlODM0ZmVmZjBmMjMyZThkM2RmNmYyMjk4MTU0YyIsIm5iZiI6MTcyNDE5NzE5NC4xODgwNjEsInN1YiI6IjYyM2JkMjkzOTQ1YzIwMDA5MDIyMDcxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.IbeMZsg1rrRtoiTbwmJfrSdN_Lazr_izEr5ARQkyHyE'
-    },
+    headers,
   };
 
   const res = await fetch(url, options);
@@ -17,4 +24,24 @@ export const fetchTopRatedMovies = async () => {
 
   const json = await res.json();
   return json.results;
+};
+
+export const fetchMovie = async (id: number) => {
+
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+  const options = {
+    method: "GET",
+    headers,
+  };
+
+
+  const res = await fetch(url, options);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch movie");
+  }
+
+  const json = await res.json();
+  return json;
+
 };
--- app/[id].tsx
+++ app/[id].tsx
@@ -1,13 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 import React from "react";
-import { useLocalSearchParams } from 'expo-router';
+import { useLocalSearchParams } from "expo-router";
+import { useQuery } from "@tanstack/react-query";
+import { fetchMovie } from '@/api/movies';
 
 const MovieDetails = () => {
   const { id } = useLocalSearchParams();
 
+  const {data, isLoading, error  } = useQuery({
+    queryKey: ["movies", id],
+    queryFn: () => fetchMovie(id),
+
+  })
+
+  if (isLoading) {
+    return <ActivityIndicator />
+  }
+
+  if(error) {
+    return <Text>Failed to fetch data</Text>
+  }
+
   return (
     <View>
-      <Text>MovieDetails: {id} </Text>
+      <Text style={{fontSize: 24, fontWeight: '500'}}>{data.title}</Text>
     </View>
   );
 };
